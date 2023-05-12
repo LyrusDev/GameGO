d91171365fb0f04fab040c22babf6562b8666a59
syonnet <vic_gen@hotmail.com>
2023-05-12 02:49:24 -0500

    nav bar ,footer,home
    integracion de img,css,js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/other/footer/footer.component';
+import { NavbarComponent } from './components/other/navbar/navbar.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { NosotrosComponent } from './components/pages/nosotros/nosotros.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavbarComponent,
+    HomeComponent,
+    NosotrosComponent
   ],
   imports: [
     BrowserModule,
