a4c25d87c5309564214154e8df69ea0e745689e2
Aldo Guizado <aldoguizado11@gmail.com>
2023-05-12 15:17:18 -0500

    Login y correcciones de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
 import { NosotrosComponent } from './components/pages/nosotros/nosotros.component';
+import { LoginComponent } from './components/pages/login/login.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  
+  { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
   { path: 'nosotros', component: NosotrosComponent },
   
-
-
+  
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './components/other/footer/footer.component';
-import { NavbarComponent } from './components/other/navbar/navbar.component';
+import { FooterComponent } from './components/others/footer/footer.component';
+import { NavbarComponent } from './components/others/navbar/navbar.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { NosotrosComponent } from './components/pages/nosotros/nosotros.component';
+import { LoginComponent } from './components/pages/login/login.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { NosotrosComponent } from './components/pages/nosotros/nosotros.componen
     FooterComponent,
     NavbarComponent,
     HomeComponent,
-    NosotrosComponent
+    NosotrosComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
